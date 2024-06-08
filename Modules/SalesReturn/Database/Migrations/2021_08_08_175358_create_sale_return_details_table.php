2a4b2a293d5e464e792f8887bad3a3125d92afdc
TharushanKBP <129367666+TharushanKBP@users.noreply.github.com>
2024-06-08 16:17:53 +0530

    sale_return update

--- Modules/SalesReturn/Database/Migrations/2021_08_08_175358_create_sale_return_details_table.php
+++ Modules/SalesReturn/Database/Migrations/2021_08_08_175358_create_sale_return_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSaleReturnPaymentsTable extends Migration
+class CreateSaleReturnDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,23 @@ class CreateSaleReturnPaymentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sale_return_payments', function (Blueprint $table) {
+        Schema::create('sale_return_details', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('sale_return_id');
-            $table->integer('amount');
-            $table->date('date');
-            $table->string('reference');
-            $table->string('payment_method');
-            $table->text('note')->nullable();
-            $table->foreign('sale_return_id')->references('id')->on('sale_returns')->cascadeOnDelete();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->string('product_name');
+            $table->string('product_code');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('unit_price');
+            $table->integer('sub_total');
+            $table->integer('product_discount_amount');
+            $table->string('product_discount_type')->default('fixed');
+            $table->integer('product_tax_amount');
+            $table->foreign('sale_return_id')->references('id')
+                ->on('sale_returns')->cascadeOnDelete();
+            $table->foreign('product_id')->references('id')
+                ->on('products')->nullOnDelete();
             $table->timestamps();
         });
     }
@@ -33,6 +41,6 @@ class CreateSaleReturnPaymentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sale_return_payments');
+        Schema::dropIfExists('sale_return_details');
     }
 }
